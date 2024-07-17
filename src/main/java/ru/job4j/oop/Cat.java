e7703525d587061ecf7adff9950a27933232de94
Аида <glubokova_aida@mail.ru>
2024-07-17 23:24:27 +0500

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,26 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String name;
+
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Имя котика: " + name + ". Он ел: рыбу.");
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        peppy.giveNick("Peppy");
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
+        peppy.show();
     }
 }
