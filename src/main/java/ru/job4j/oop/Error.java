4fe6ca63f8c341d62ddfd2153d5435acf38073a1
Аида <glubokova_aida@mail.ru>
2024-07-18 17:40:54 +0500

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error(true, 66, "Error");
+        error.printInfo();
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message:" + message);
+    }
+}
