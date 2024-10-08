6df0390cbca508b3c34e2d0c6611c64390775ef7
Аида <glubokova_aida@mail.ru>
2024-10-08 18:26:35 +0500

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -53,4 +53,14 @@ public class Tracker {
         }
         return false;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            size--;
+            return true;
+        }
+        return false;
+    }
 }
