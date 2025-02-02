33bb5a0f19707306ffb16c439af2ec73cd3a1426
Аида <glubokova_aida@mail.ru>
2025-02-02 18:48:02 +0500

    2.1. Scanner и чтение числа из консоли. [#179518]

--- src/main/java/ru/job4j/concat/ConcatExemple.java
+++ src/main/java/ru/job4j/concat/ConcatExemple.java
@@ -2,7 +2,7 @@ package ru.job4j.concat;
 
 public class ConcatExemple {
     public static void main(String[] args) {
-        String string = "abc".concat("def").concat("ghi").concat("jkl");
+        String string = "abc".concat("deff").concat("ghi").concat("jkl");
         System.out.println(string);
     }
 }
