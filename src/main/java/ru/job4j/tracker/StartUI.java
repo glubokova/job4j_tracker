6f6489618fa4a3e2f993f5bc7056828c65c5cf3a
Аида <glubokova_aida@mail.ru>
2025-02-02 18:56:45 +0500

    3.1. Реализация класса StartUI. Добавление заявки [#500744 #529808]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -12,7 +12,7 @@ public class StartUI {
             int select = Integer.parseInt(scanner.nextLine());
             if (select == 0) {
                 System.out.println("=== Создание новой заявки ===");
-                System.out.print("Введите имя: ");
+                System.out.print("Введите имяя: ");
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item); // Добавляем заявку в трекер
