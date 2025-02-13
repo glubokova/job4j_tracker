ceb36151fc50db80e7d49c0b4ef7ee31bb65f3ba
Аида <glubokova_aida@mail.ru>
2025-02-13 21:47:03 +0500

    3.1. Реализация класса StartUI. Добавление заявки [#500744 #529808]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -24,11 +24,11 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", created=" + created +
-                '}';
+        return "Item{"
+                + "id=" + id
+                + ", name='" + name + '\''
+                + ", created=" + created
+                + '}';
     }
 
     public int getId() {
