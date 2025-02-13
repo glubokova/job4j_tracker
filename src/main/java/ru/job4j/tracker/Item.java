58c36ee46dc41a832a71107520d72848d58765de
Аида <glubokova_aida@mail.ru>
2025-02-13 21:44:02 +0500

    3.1. Реализация класса StartUI. Добавление заявки [#500744 #529808]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,21 +1,34 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created;
 
     public Item() {
+        created = LocalDateTime.now();
     }
 
     public Item(String name) {
         this.name = name;
+        created = LocalDateTime.now();
     }
 
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        created = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
     }
 
     public int getId() {
