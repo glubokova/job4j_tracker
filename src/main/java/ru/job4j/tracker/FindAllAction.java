a4c30ea9bca4f18aa30f2edb10253ce221495d85
Аида <glubokova_aida@mail.ru>
2025-03-09 12:48:10 +0500

    11.1. Реализация меню за счет шаблона стратегия. [#181780]

---
+++ src/main/java/ru/job4j/tracker/FindAllAction.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+public class FindAllAction implements UserAction {
+    @Override
+    public String name() {
+        return "Показать все заявки";
+    }
+
+    @Override
+    public boolean execute(Input input, Tracker tracker) {
+        System.out.println("=== Вывод всех заявок ===");
+        Item[] items = tracker.findAll();
+        for (Item item : items) {
+            System.out.println(item);
+        }
+        return true;
+    }
+}
